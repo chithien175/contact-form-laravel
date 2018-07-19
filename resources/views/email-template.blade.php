fa033ef046da1f38096759b8e3e4946f4b9b3aa1
Thien Pham <chithien175@gmail.com>
2018-07-19 11:24:34 +0700

    update 1907_2

--- resources/views/email-template.blade.php
+++ resources/views/email-template.blade.php
@@ -1,25 +1,37 @@
 Bạn đã nhận được một tin nhắn từ : {{ $contact['fullname'] }}
- 
+
+@if(isset($contact['fullname'])) 
 <p>
 Tên: {{ $contact['fullname'] }}
 </p>
- 
+@endif 
+
+@if(isset($contact['email'])) 
 <p>
 Email: {{ $contact['email'] }}
 </p>
+@endif 
 
+@if(isset($contact['address'])) 
 <p>
 Địa chỉ: {{ $contact['address'] }}
 </p>
+@endif 
 
+@if(isset($contact['phone'])) 
 <p>
 Điện thoại: {{ $contact['phone'] }}
 </p>
+@endif 
 
+@if(isset($contact['subject'])) 
 <p>
 Chủ đề: {{ $contact['subject'] }}
 </p>
- 
+@endif 
+
+@if(isset($contact['msg'])) 
 <p>
 Nội dung: {{ $contact['msg'] }}
 </p>
+@endif 
